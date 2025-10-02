a497b61f5cf8800e14751ba779a816f216ee17d4
Alexander Grachev <sashagrachev2005@gmail.com>
2025-10-02 11:08:33 +0300

    Added completed scripts

---
+++ completed_scripts_jetbot/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+
+from setuptools import setup
+package_name = 'completed_scripts_jetbot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob("launch/*.launch.py"),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob("config/*.yaml"),
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='banana-killer',
+    maintainer_email='sashagrachev2005@gmail.com',
+    description='This package include launch-files, what state the full system of jetbot from one click',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
